2d3cb4371bae20b6d2d4af311a97910a0032bf9a
Alexandria <alex.pugia816@hotmail.com>
2019-11-20 15:31:42 -0500

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_192207) do
+ActiveRecord::Schema.define(version: 2019_11_20_192011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "statuses", force: :cascade do |t|
-    t.integer "user_id"
-    t.text "content"
-    t.integer "hearts", default: 0
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
