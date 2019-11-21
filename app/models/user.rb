1aec8378004d710582a71db0baf7d084fa59e1a1
Alexandria <alex.pugia816@hotmail.com>
2019-11-21 10:03:06 -0500

    added followers table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    has_many :statuses
+    has_many :follows
+    has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+    has_many :followers, through: :follower_relationships, source: :follower
+    has_many :following_relationships, foreign_key: :user_id, class_name: 'Follow'
+    has_many :following, through: :following_relationships, source: :following
 
     validates :username, presence: true, uniqueness: { case_sensitive: false }
     has_secure_password
