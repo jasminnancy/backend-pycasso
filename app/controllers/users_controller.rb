06da26590fe1d6051578447e6e0c7fddfefbac77
Alexandria <alex.pugia816@hotmail.com>
2019-11-26 09:53:44 -0500

    fixed delete button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
    
     def create
         @user = User.create(user_params)
+
         if @user.valid?
             @token = encode_token({ user_id: @user.id })
             render json: { user: @user, jwt: @token }, status: :created
